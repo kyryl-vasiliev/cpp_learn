21aa74671458dc02e6fb117fc9b00ca37de0a513
KyrylVasilev <kyryl.vasiliev@gmail.com>
2020-12-26 02:29:30 +0200

    basic files for dyn lib loading

---
+++ linlibuse/sharedlib.cpp
@@ -0,0 +1,4 @@
+int testFunction2()
+{
+	return 999;
+}
---
+++ linlibuse/usedynlib.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int testFunction2();
+int main(int argc, char**argv)
+{
+	std::cout << testFunction2();
+	return 0;
+}
